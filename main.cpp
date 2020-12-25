22ad5658ff3045f4428ea1266f976b6d81657ab8
Dhruv-m-Shah <shahdhruv2001@gmail.com>
2020-12-25 02:10:11 -0500

    Working .wasm file, cpp code compiling, and running on browser (currently tested on firefox

--- main.cpp
+++ main.cpp
@@ -6,14 +6,15 @@
 using namespace std;
 				
 int main(){
-	string test = "start num x=1+1? end";
+	string test = "start num x=4353452435234523453451*13453245234537245432589? end";
 	vector<TOKEN> temp;
 	tokenize(test, temp);
 }
 
 extern "C"{
-	void interpretorWrapper(string code){
+	void interpretorWrapper(char * code){
+		string str = string(code);
 		vector<TOKEN> temp;
-		tokenize(code, temp);
+		tokenize(str, temp);
 	}
 }
